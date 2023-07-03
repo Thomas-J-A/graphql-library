fc7d633e39443aae3e39fa5f37ffbc965e8533ce
Thomas Aspbury <tomaspbury@icloud.com>
2023-07-03 08:57:27 +0100

    Complete exercise 8.25

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -8,7 +8,7 @@ import { NotificationContext } from './contexts/NotificationContext';
 import MainNav from './components/MainNav/MainNav';
 import Notification from './components/UI/Notification/Notification';
 
-import { BOOK_ADDED_SUBSCRIPTION } from './queries';
+import { BOOK_ADDED_SUBSCRIPTION, GET_BOOKS_QUERY } from './queries';
 
 import * as S from './App.styled';
 
@@ -17,9 +17,27 @@ const App = () => {
   const [notificationMsg, setNotificationMsg] = useState('');
 
   useSubscription(BOOK_ADDED_SUBSCRIPTION, {
-    onData: ({ data }) => {
-      console.log(data);
-      alert('Data received!');
+    onData: ({ data, client }) => {
+      const { bookAdded } = data.data;
+
+      // Show user a temporary notification
+      setNotificationMsg(
+        `New book added: ${bookAdded.title} by ${bookAdded.author.name}`,
+      );
+      setTimeout(() => setNotificationMsg(''), 5000);
+
+      // Update cache field used by books list
+      client.cache.updateQuery(
+        { query: GET_BOOKS_QUERY, variables: { genre: '' } },
+        ({ allBooks }) => ({
+          allBooks: [...allBooks, bookAdded],
+        }),
+      );
+
+      // Update cache field used by genre list
+      client.cache.updateQuery({ query: GET_BOOKS_QUERY }, ({ allBooks }) => ({
+        allBooks: [...allBooks, bookAdded],
+      }));
     },
   });
 
